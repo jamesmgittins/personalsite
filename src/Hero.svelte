3182265f4a82a950d81d0ae8f55b8fee4802c3ca
jamesmgittins <jamesmgittins@gmail.com>
2021-01-19 20:42:48 +0000

    rebuild

--- src/Hero.svelte
+++ src/Hero.svelte
@@ -13,7 +13,7 @@
 <style>
     .hero {width:100%;height:100vh;overflow:hidden;margin-bottom:25px;position:relative;display:table;font-weight:400;}
     .hero > .table-cell {display:table-cell;vertical-align:middle;text-align:center;padding:25px;}
-    .hero > .table-cell > h1 {font-size:5rem;margin-top: 0;}
+    .hero > .table-cell > h1 {font-size:5rem; margin-top: 0;}
     .hero > .table-cell > h2 {font-size:2rem;}
     .hero h1, .hero h2 {color:#ccc;font-weight: 400;}
 </style>
