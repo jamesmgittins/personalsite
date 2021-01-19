ae9c7f0a6a947f5789aa15ab9f6c530e9142b495
jamesmgittins <jamesmgittins@gmail.com>
2021-01-19 20:22:07 +0000

    Added a transition for the canvas

--- src/App.svelte
+++ src/App.svelte
@@ -12,16 +12,31 @@
 		font-family: 'Raleway', sans-serif;
 		font-size:16px;
 		margin:0;
-		background-color: darkgreen;
 	}
-	:global(canvas) {
+	:global(#canvas-container) {
 		height:100% !important;
 		position:fixed;
 		bottom:0;
 		left:0;
 		z-index: -10;
 	}
-	/* :global(html) {
-		background-color: #ccc;
-	} */
+	:global(#canvas-container::before, #canvas-container::after) {
+		content: "";
+		display: block;
+		position: absolute;
+		background-color: rgb(31, 102, 31);
+		width:50%;
+		height:100%;
+		top:0;
+		transition: width 4s ease-in-out;
+	}
+	:global(#canvas-container::before) {
+		left:0;
+	}
+	:global(#canvas-container::after) {
+		right:0;
+	}
+	:global(#canvas-container.ready::before, #canvas-container.ready::after) {
+		width: 0;
+	}
 </style>
